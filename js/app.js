4ca76faeb667a7de085ace375baa4533319958d3
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2016-07-20 14:16:36 +0800

    add file app.js

---
+++ js/app.js
@@ -0,0 +1,4 @@
+import React from 'react' ;
+import { render } from 'react-dom' ;
+const myDivElement = <div className="foo">Todo app</div>;
+render(myDivElement, document.getElementById('todoapp'));
